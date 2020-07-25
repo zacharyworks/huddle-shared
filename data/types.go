82e0b74b7abc1f56f47683f5d838d9b069a2404e
Zachary Briggs <zacbriggs@outlook.com>
2020-07-25 18:15:24 +0100

    adding back action

--- data/types.go
+++ data/types.go
@@ -6,3 +6,10 @@ type StringAction struct {
 	ActionType    string `json:"ActionType"`
 	ActionPayload string `json:"ActionPayload"`
 }
+
+// StringAction an action with a string as the payload
+type Action struct {
+	ActionSubset  string `json:"ActionSubset"`
+	ActionType    string `json:"ActionType"`
+	ActionPayload interface{} `json:"ActionPayload"`
+}

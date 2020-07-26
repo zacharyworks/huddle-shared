7b3ee618a4fb4269ca281d812c8fd55c2c86edb0
Zachary Briggs <zacbriggs@outlook.com>
2020-07-26 12:11:31 +0100

    refactor so db credentials can be provided by calling function

--- db/db.go
+++ db/db.go
@@ -4,16 +4,33 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"strings"
 )
 
+type Credentials struct {
+	DbURL string
+	DbPort string
+	DbName string
+	DbUser string
+	DbPass string
+}
 // DbCon connection
 var DbCon *sql.DB
 
 // ConnectDB Connects to database
-func ConnectDB() {
+func ConnectDB(cred Credentials) {
+	var db strings.Builder
+	db.WriteString(cred.DbUser)
+	db.WriteString(":")
+	db.WriteString(cred.DbPass)
+	db.WriteString("@tcp(")
+	db.WriteString(cred.DbURL)
+	db.WriteString(":")
+	db.WriteString(cred.DbPort)
+	db.WriteString(")/")
+	db.WriteString(cred.DbName)
 	var err error
-	DbCon, err = sql.Open("mysql",
-		"monty:python@tcp(127.0.0.1:3306)/huddle")
+	DbCon, err = sql.Open("mysql", db.String())
 
 	if err != nil {
 		log.Fatal(err)
